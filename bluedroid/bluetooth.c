fb22606d2e4e9b254851430aea565aba470756ef
Alan Marchesan <justcavalli@gmail.com>
2012-09-13 14:22:28 +0200

    start scanning from rfkill1 to avoid allwinner's 'fake' bluetooth

--- bluedroid/bluetooth.c
+++ bluedroid/bluetooth.c
@@ -52,7 +52,7 @@ static int init_rfkill() {
     int fd;
     int sz;
     int id;
-    for (id = 0; ; id++) {
+    for (id = 1; ; id++) {
         snprintf(path, sizeof(path), "/sys/class/rfkill/rfkill%d/type", id);
         fd = open(path, O_RDONLY);
         if (fd < 0) {
